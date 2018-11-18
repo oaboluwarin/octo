5d0297af7edea19a3ad7a1282081bd1323da1e06
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-18 13:17:19 +0100

    add jest config to eslintrc

---
+++ server/__tests__/routes.spec.js
@@ -0,0 +1,30 @@
+import supertest from 'supertest';
+import app from '../app';
+
+const request = supertest(app);
+
+describe('API', () => {
+  describe('The home route', () => {
+    it('Returns an appropriate welcome message', (done) => {
+      request.get('/')
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
+          expect(res.body.message).toBe('Welcome to octo');
+          done();
+        });
+    });
+  });
+
+  describe('/user', () => {
+    it('fetches all the users on the /user/all route', (done) => {
+      request.get('/user/all')
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
+          expect(res.body.length).toBe(3);
+          done();
+        });
+    });
+  });
+});

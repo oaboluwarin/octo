e8ecb9971762ba30b4fe692dd6dd09b688a44fbe
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-19 19:52:27 +0100

    correct mongoURI error

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 const databaseFunction = () => {
   if (process.env.NODE_ENV === 'production') {
-    return { monogURI: 'mongodb://oreoluwade:aster01d@ds229648.mlab.com:29648/octojot-prod' };
+    return { mongoURI: 'mongodb://oreoluwade:aster01d@ds229648.mlab.com:29648/octojot-prod' };
   }
   return { mongoURI: 'mongodb://localhost/octojot-dev' };
 };

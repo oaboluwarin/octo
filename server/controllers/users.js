7120e4045a0091f2e9e327e7f3ae15c66293908d
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-19 16:00:38 +0100

    refine logic for registering a user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-param-reassign */
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+// import passport from 'passport';
 import '../models/User';
 import jsonData from '../helpers/users';
 
@@ -15,7 +17,58 @@ export default {
   login(req, res) {
     res.render('users/login');
   },
-  register(req, res) {
+  registerForm(req, res) {
     res.render('users/register');
+  },
+  register(req, res) {
+    const errors = [];
+    const {
+      email,
+      name,
+      password,
+      password2,
+    } = req.body;
+
+    if (password !== password2) errors.push({ text: 'Passwords do not match' });
+    if (password.length < 4) errors.push({ text: 'Password must be at least 4 characters '});
+    if (!name) errors.push({ text: 'The name field should not be empty' });
+    if (!email) errors.push({ text: 'Please, supply a valid email' });
+
+    if (errors.length) {
+      return res.render('users/register', {
+        errors,
+        name,
+        email,
+        password,
+        password2
+      });
+    }
+    return User.findOne({
+      email
+    })
+      .then(user => {
+        if (user) {
+          req.flash('error_msg', 'Email already registered');
+          res.redirect('/users/login');
+        }
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+
+        bcrypt.genSalt(10, (error, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => {
+                req.flash('success_msg', 'Registration Successful!');
+                res.redirect('/users/login');
+              })
+              .catch(errs => console.log(errs));
+          });
+        });
+      });
   }
-}
+};

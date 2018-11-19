f5a69148adc34d99a094c4d6fbceda9a08b99a5c
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-19 11:55:26 +0100

    add an idea model

---
+++ server/models/Idea.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose';
+
+// Create Schema
+const IdeaSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model('ideas', IdeaSchema);

1f2405b3134b3d5dc485528508ca70161a596775
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-19 08:03:30 +0100

    add handlebars integration

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import logger from 'morgan';
+import exphbs  from 'express-handlebars';
 import { userRouter } from './routes';
 
 const app = express();
 
 app.use(logger('dev'));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
   return res.json({ message: 'Welcome to octo' });
